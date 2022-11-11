5b76ebd0e9c4c8481de9884e8e104f227f9522cd
Rick welte <rktvpr@gmail.com>
2022-11-11 14:10:18 -0500

    prepped for HTML calls with template literals and added JQuery and Bootstrap to bottom of base HTML

---
+++ src/teamGenerator.js
@@ -0,0 +1,29 @@
+//functions to call HTML into dist folder with template literals
+const managerSection = function (manager) {
+    //insert HTML
+}
+
+const engineerSection = function (engineer) {
+    //insert HTML
+}
+
+const internsSection = function (intern) {
+    //insert HTML
+}
+
+const generateHTML = function (data) {
+    //generate HTML here
+}
+
+const generateWebpage = function () {
+    //add base html here
+
+
+
+
+
+
+    //adds bootstrap and jquery to HTML
+    `<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>`
+}
